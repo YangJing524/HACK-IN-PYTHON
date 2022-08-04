04c4696bd6194ac9a7c5ccbe0d0083ad97bb1d58
杨景 <2508204932@qq.com>
2022-08-04 22:20:58 +0800

    添加脚本

---
+++ 探测你的电脑去过哪里.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+from scapy.layers.dot11 import Dot11ProbeReq
+
+interfacs='eth0'
+probeReqs=[]
+
+def sniffProbe(p):
+	if p.haslayer(Dot11ProbeReq):
+		netName=p.getlayer(Dot11ProbeReq).info
+		if netName not in probeReqs:
+			probeReqs.append(netName)
+			print('[+] Detect New probeRequest:'+netName)
+
+sniff(iface=interfacs,prn=sniffProbe)
+
